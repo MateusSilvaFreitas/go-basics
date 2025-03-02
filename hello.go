31872f72f6c5f5467beb59d8659370a35acf739e
Mateus Silva Freitas <mateus.freitas7589@gmail.com>
2025-03-02 20:08:37 -0300

    Initial data commit

---
+++ hello.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hi")
+	var a = 0;
+
+	if(a > 0){
+		fmt.Println("é maior que zero");
+	} else {
+		fmt.Println("é menor ou igual a zero")
+	}
+}
